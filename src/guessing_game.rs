2a2481eac153f09ef0f81e6589f74b9481b194d9
Nazmul Idris <nazmul@fasterlap.com>
2022-01-20 16:59:47 -0800

    Update guessing_game with use of tuple

--- src/guessing_game.rs
+++ src/guessing_game.rs
@@ -1,9 +1,9 @@
 use std::cmp::Ordering;
-use std::io::stdin;
 
 use ansi_term::Colour::Blue;
 use rand::{Rng, thread_rng};
 
+use crate::utils;
 use crate::utils::{print_header, style_dimmed, style_error, style_primary, style_prompt};
 
 /// Rust book - https://doc.rust-lang.org/book/ch02-00-guessing-game-tutorial.html
@@ -25,23 +25,14 @@ pub fn run() {
 /// String not &str due to "struct lifetime" - https://stackoverflow.com/a/29026565/2085356
 fn make_a_guess() -> String {
   println!("{}", Blue.paint("Please input your guess."));
-  let mut guess: String = String::new();
-  // https://learning-rust.github.io/docs/e4.unwrap_and_expect.html
-  match stdin().read_line(&mut guess) {
-    Ok(bytes_read) => {
-      let guess: String = guess.trim().to_string(); // Remove any whitespace (including \n).
-      println!("{} {}, {} {}",
-        style_dimmed("#bytes read:"),
-        style_primary(&bytes_read.to_string()),
-        style_dimmed("You guessed:"),
-        style_primary(&guess));
-      guess
-    }
-    Err(_) => {
-      println!("{}", style_error("Something went wrong when reading input from terminal."));
-      "".to_string()
-    }
-  }
+  let (bytes_read, guess) = utils::readline();
+  println!("{} {}, {} {}",
+    style_dimmed("#bytes read:"),
+    style_primary(&bytes_read.to_string()),
+    style_dimmed("You guessed:"),
+    style_primary(&guess)
+  );
+  guess
 }
 
 fn match_guess(answer: &u32, guess: &String) {
--- src/utils.rs
+++ src/utils.rs
@@ -1,6 +1,7 @@
 use ansi_term::ANSIGenericString;
 use ansi_term::Color::{Blue, Green, Red, White};
 use ansi_term::Colour::Purple;
+use std::io::stdin;
 
 /// ANSI colorized text: /// https://github.com/ogham/rust-ansi-term
 /// Equivalent for template string literal. One way to do this using format!
@@ -37,4 +38,20 @@ pub fn style_error(text: &str) -> ANSIGenericString<str> {
 
 pub fn style_dimmed(text: &str) -> ANSIGenericString<str> {
   return White.underline().paint(text);
+}
+
+/// Return String not &str due to "struct lifetime" - https://stackoverflow.com/a/29026565/2085356
+pub fn readline() -> (usize, String) {
+  let mut temp_string_buffer: String = String::new();
+  // https://learning-rust.github.io/docs/e4.unwrap_and_expect.html
+  match stdin().read_line(&mut temp_string_buffer) {
+    Ok(bytes_read) => {
+      let guess: String = temp_string_buffer.trim().to_string(); // Remove any whitespace (including \n).
+      (bytes_read, guess)
+    }
+    Err(_) => {
+      println!("{}", style_error("Something went wrong when reading input from terminal."));
+      (0, "".to_string())
+    }
+  }
 }
