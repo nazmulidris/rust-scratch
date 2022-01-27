47199ae76b561f877fa50f3e9c987572856b4c32
Nazmul Idris <nazmul@fasterlap.com>
2022-01-27 12:24:21 -0800

    Add copyright config and copyrights

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,26 @@
+/*
+ * Copyright (c) 2022 Nazmul Idris. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 use std::io::stdin;
 
 use ansi_term::ANSIGenericString;
 use ansi_term::Color::{Blue, Green, Red, White};
 use ansi_term::Colour::Purple;
 
+/// Test block example.
 #[cfg(test)]
 mod tests {
   #[test]
@@ -11,6 +28,16 @@ mod tests {
     assert_eq!(2 + 2, 4);
   }
 }
+
+/// Single test case.
+#[test]
+fn test_something() {
+  let tuple1: (i32, String) = (100, "123".to_string());
+  let (number, text) = tuple1;
+  assert_eq!(number, 100);
+  assert_eq!(text, "123");
+}
+
 
 /// ANSI colorized text:
 /// - https://github.com/ogham/rust-ansi-term
