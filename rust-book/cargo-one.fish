cde54c82b0735a4785292ad8453329457669895e
Nazmul Idris <nazmul@fasterlap.com>
2022-02-19 19:28:44 -0800

    [rust-book] Update fish scripts

--- rust-book/cargo-one.fish
+++ rust-book/cargo-one.fish
@@ -8,11 +8,15 @@ if test -z "$argv"
     exit 1
 end
 
-# Make sure to install cargo-watch via `cargo install cargo-watch`.
+# 1. Make sure to install cargo-watch via `cargo install cargo-watch`.
 # More info about cargo-watch: https://crates.io/crates/cargo-watch
+
+# 2. Make sure to install cargo-limit via `cargo install cargo-limit`.
+# More info about carg-limit: https://crates.io/crates/cargo-limit
 
 # More info about cargo test: https://doc.rust-lang.org/book/ch11-02-running-tests.html
 # 2 sets of options:
 # 1. cargo test --help      => these go to the cargo test command
 # 2. cargo test -- --help   => these go to the binary that is being tested
-cargo watch -x check -x "test $argv --color always -q --message-format short  -- --nocapture" -c -q
+
+cargo watch -x check -x "ltest $argv --color always -q --message-format short  -- --nocapture" -c -q
