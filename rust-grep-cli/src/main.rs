58be7038f60903dc8083f52429815476ab118180
Nazmul Idris <nazmul@fasterlap.com>
2022-03-01 18:19:47 -0800

    [rust-grep-cli] Move tty_utils to r3bl_rs_utils

--- rust-grep-cli/src/main.rs
+++ rust-grep-cli/src/main.rs
@@ -1,33 +1,31 @@
-//! This program has 2 modes of operation.
-//!
-//! # Mode 1 - Not using `stdin` (input is not piped into this program).
-//!
-//! <search-string> <path-to-file> <case-sensitive>
-//! ↑               ↑              ↑          
-//! arg 0           arg 0          arg 0   
-//!
-//! # Mode 2 - Using `stdin` (input is piped into this program).
-//!
-//! Content is piped into the program via `stdin` and it only needs to take the search string and
-//! the case sensitive matching as arguments.
-
 // Connect to Rust source files.
 mod grep;
 mod grep_command_builder;
 mod piped_grep;
 mod piped_grep_command_builder;
-mod tty_utils;
 
 // Imports.
 use grep::grep;
 use grep_command_builder::GrepOptionsBuilder;
 use piped_grep::piped_grep;
 use piped_grep_command_builder::PipedGrepOptionsBuilder;
-use r3bl_rs_utils::utils::style_error;
+use r3bl_rs_utils::utils::{is_stdin_piped, style_error};
 use std::env::args;
 use std::error::Error;
-use tty_utils::is_stdin_piped;
 
+/// This program has 2 modes of operation.
+///
+/// # Mode 1 - Not using `stdin` (input is not piped into this program).
+///
+/// <search-string> <path-to-file> <case-sensitive>
+///       ↑               ↑              ↑          
+///     arg 0           arg 0          arg 0   
+///
+/// # Mode 2 - Using `stdin` (input is piped into this program).
+///
+/// Content is piped into the program via `stdin` and it only needs to take the search string and
+/// the case sensitive matching as arguments.
+///
 /// More info on `Box<dyn Error>` or `&'static dyn Error`:
 /// - `'static` is the lifetime of `Box<dyn Error>`.
 /// - <https://users.rust-lang.org/t/what-does-it-mean-to-return-dyn-error-static/37619/7>
