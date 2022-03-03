11142c492f136ca1db6189996959190d17605982
Nazmul Idris <nazmul@fasterlap.com>
2022-03-03 11:15:32 -0800

    [address-book-with-redux] WIP

--- address-book-with-redux/src/main.rs
+++ address-book-with-redux/src/main.rs
@@ -1,9 +1,18 @@
-use std::error::Error;
+use std::{env::args, error::Error, process::exit};
+
+use r3bl_rs_utils::utils::{style_error, with};
 
 fn main() {
-  
+  let args = args().collect::<Vec<String>>();
+  with(run(args), |it| match it {
+    Ok(()) => exit(0),
+    Err(err) => {
+      eprintln!("{}: {}", style_error("Problem encountered"), err);
+      exit(1);
+    }
+  });
 }
 
-fn run() -> Result<(), Box<dyn Error>> {
+fn run(args: Vec<String>) -> Result<(), Box<dyn Error>> {
   Ok(())
 }
