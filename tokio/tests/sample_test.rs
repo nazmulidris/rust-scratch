f60d7fcdef3e353c06321c84b97e5d8b074a90c9
Nazmul Idris <nazmul@fasterlap.com>
2022-03-11 15:25:54 -0800

    [tokio] - Add complex test w/ middleware function that captures shared variables

--- tokio/tests/sample_test.rs
+++ tokio/tests/sample_test.rs
@@ -1,4 +1,9 @@
-use tokio_example_lib::my_middleware::{adder_mw, logger_mw, Action};
+use std::sync::{Arc, Mutex};
+
+use tokio_example_lib::{
+  middleware::SafeFnWrapper,
+  my_middleware::{adder_mw, logger_mw, Action},
+};
 
 // Integration tests
 // Functions that are in scope: <https://stackoverflow.com/a/45151641/2085356>
@@ -15,4 +20,23 @@ async fn test_logger_mw_works() {
 async fn test_adder_mw_works() {
   let result = adder_mw().spawn(Action::Add(1, 2)).await.unwrap();
   assert_eq!(result, Some(Action::Result(3)));
+}
+
+#[tokio::test]
+async fn test_complex_mw_example_works() {
+  let stack = Arc::new(Mutex::new(vec![]));
+  let stack_ref = stack.clone();
+  let adder_lambda = move |action: Action| match action {
+    Action::Add(a, b) => {
+      let sum = a + b;
+      let mut stack_ref = stack_ref.lock().unwrap();
+      stack_ref.push(a + b);
+      Some(Action::Result(sum))
+    }
+    _ => None,
+  };
+  let foo = SafeFnWrapper::new(adder_lambda);
+  let result = foo.spawn(Action::Add(1, 2)).await.unwrap();
+  assert_eq!(result, Some(Action::Result(3)));
+  assert_eq!(stack.lock().unwrap().len(), 1);
 }
