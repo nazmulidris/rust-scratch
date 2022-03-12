8035a0037f74d6e45dd405f0fde483c15c887524
Nazmul Idris <nazmul@fasterlap.com>
2022-03-12 10:37:27 -0800

    [tokio] - Add copyright headers

--- tokio/src/main.rs
+++ tokio/src/main.rs
@@ -1,16 +1,32 @@
-use tokio_example_lib::my_middleware::{adder_mw, logger_mw, Action};
+/*
+ Copyright 2022 Nazmul Idris
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+      https://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+*/
 
 // Imports.
+use tokio_example_lib::{my_middleware::{adder_mw, logger_mw, Action}, middleware::SafeFnWrapper};
+
 #[tokio::main]
 async fn main() {
   {
-    let mw_fun = logger_mw();
+    let mw_fun: SafeFnWrapper<Action> = logger_mw();
     mw_fun.spawn(Action::Add(1, 2)).await.unwrap();
     mw_fun.spawn(Action::Add(1, 2)).await.unwrap();
   }
 
   {
-    let mw_fun = adder_mw();
+    let mw_fun: SafeFnWrapper<Action> = adder_mw();
     println!("{:?}", mw_fun.spawn(Action::Add(1, 2)).await.unwrap());
     println!("{:?}", mw_fun.spawn(Action::Add(1, 2)).await.unwrap());
   }
--- tokio/src/middleware.rs
+++ tokio/src/middleware.rs
@@ -1,3 +1,19 @@
+/*
+ Copyright 2022 Nazmul Idris
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+      https://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+*/
+
 // Imports.
 use std::{
   marker::{Send, Sync},
@@ -18,6 +34,8 @@ pub type SafeFn<A> = Arc<RwLock<dyn FnMut(A) -> Option<A> + Sync + Send>>;
 pub struct SafeFnWrapper<A> {
   fn_mut: SafeFn<A>,
 }
+
+pub type Future<T> = JoinHandle<T>;
 
 impl<A: Sync + Send + 'static> SafeFnWrapper<A> {
   pub fn new(
@@ -47,5 +65,3 @@ impl<A: Sync + Send + 'static> SafeFnWrapper<A> {
     })
   }
 }
-
-pub type Future<T> = JoinHandle<T>;
