d5060795d08ae92c203ecc39beb96e227342c556
Nazmul Idris <nazmul@fasterlap.com>
2022-06-07 17:50:17 -0700

    [tui-layout-crossterm] Use crossterm EventStream for async event read

--- tui-layout-crossterm/src/module_crossterm.rs
+++ tui-layout-crossterm/src/module_crossterm.rs
@@ -51,10 +51,34 @@
 //! - https://github.com/crossterm-rs/crossterm/wiki/Upgrade-from-0.13-to-0.14#111-new-event-api
 //! - https://github.com/crossterm-rs/crossterm/blob/master/examples/event-stream-tokio.rs
 
+use async_trait::async_trait;
 use crossterm::event::*;
 use futures_util::{FutureExt, StreamExt};
 use r3bl_rs_utils::*;
 use tui_layout_crossterm::*;
+
+#[async_trait]
+pub trait EventStreamExt {
+  /// Read an [Event] from the [EventStream]. This is a non-blocking call.
+  async fn get_event_from(&mut self) -> CommonResult<Option<InputEvent>>;
+}
+
+#[async_trait]
+impl EventStreamExt for EventStream {
+  async fn get_event_from(&mut self) -> CommonResult<Option<InputEvent>> {
+    let option_result_event = self.next().fuse().await;
+    match option_result_event {
+      Some(Ok(event)) => Ok(Some(event.into())),
+
+      Some(Err(e)) => {
+        log!(ERROR, "Error: {:?}", e);
+        Ok(None)
+      }
+
+      None => Ok(None),
+    }
+  }
+}
 
 pub async fn emit_crossterm_commands() -> CommonResult<()> {
   raw_mode! { repl_nonblocking().await? }
@@ -64,17 +88,12 @@ async fn repl_nonblocking() -> CommonResult<()> {
   throws!({
     let mut event_stream = EventStream::new();
     loop {
-      match event_stream.next().fuse().await {
-        Some(Ok(event)) => {
-          let input_event: InputEvent = event.into();
-          if handle_input_event(input_event).await.is_err() {
+      let input_event = event_stream.get_event_from().await?;
+      match input_event {
+        Some(input_event) => {
+          if handle_input_event(input_event).await? {
             break;
-          };
-        }
-
-        Some(Err(e)) => {
-          let msg = format!("Error: {:?}\r", e);
-          println_raw!(msg);
+          }
         }
 
         None => break,
@@ -89,34 +108,33 @@ const EXIT_KEYS: [crossterm::event::KeyEvent; 1] = [KeyEvent {
   modifiers: KeyModifiers::CONTROL,
 }];
 
-/// Returns error if user presses any of the keys in [EXIT_KEYS].
-async fn handle_input_event(input_event: InputEvent) -> CommonResult<()> {
-  throws!({
-    match input_event {
-      // Check for REPL exit.
-      InputEvent::NonDisplayableKeypress(key_event) => {
-        if EXIT_KEYS.contains(&key_event) {
-          return CommonError::new_err_with_only_type(CommonErrorType::ExitLoop);
-        }
-        let KeyEvent { modifiers, code } = key_event;
-        log!(INFO, "KeyEvent: {:?} + {:?}", modifiers, code);
+/// Returns false if user presses any of the keys in [EXIT_KEYS].
+async fn handle_input_event(input_event: InputEvent) -> CommonResult<bool> {
+  match input_event {
+    // Check for REPL exit.
+    InputEvent::NonDisplayableKeypress(key_event) => {
+      if EXIT_KEYS.contains(&key_event) {
+        return Ok(true);
       }
-
-      InputEvent::DisplayableKeypress(character) => {
-        log!(INFO, "DisplayableKeypress: {:?}", character);
-      }
-
-      InputEvent::Resize(Size { height, width }) => {
-        log!(INFO, "Resize: {:?}", (height, width));
-      }
-
-      InputEvent::Mouse(mouse_event) => {
-        log!(INFO, "Mouse: {:?}", mouse_event);
-      }
-
-      _ => {
-        log!(INFO, "Other: {:?}", input_event);
-      }
+      let KeyEvent { modifiers, code } = key_event;
+      log!(INFO, "KeyEvent: {:?} + {:?}", modifiers, code);
     }
-  })
+
+    InputEvent::DisplayableKeypress(character) => {
+      log!(INFO, "DisplayableKeypress: {:?}", character);
+    }
+
+    InputEvent::Resize(Size { height, width }) => {
+      log!(INFO, "Resize: {:?}", (height, width));
+    }
+
+    InputEvent::Mouse(mouse_event) => {
+      log!(INFO, "Mouse: {:?}", mouse_event);
+    }
+
+    _ => {
+      log!(INFO, "Other: {:?}", input_event);
+    }
+  }
+  Ok(false)
 }
