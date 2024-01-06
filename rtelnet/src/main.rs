c08caf7445a14188f17d349bb97551aad76951c5
Nazmul Idris <nazmul@fasterlap.com>
2024-01-06 15:32:15 -0800

    [rtelnet] Initial commit

---
+++ rtelnet/src/main.rs
@@ -0,0 +1,205 @@
+/*
+ *   Copyright (c) 2024 Nazmul Idris
+ *   All rights reserved.
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
+
+use clap::{Parser, Subcommand};
+use std::{
+    io::{stdin, BufRead, BufReader, BufWriter, Write},
+    net::{IpAddr, TcpListener, TcpStream},
+};
+
+use type_aliases::*;
+mod type_aliases {
+    pub type IOResult<T> = std::io::Result<T>;
+}
+
+use defaults::*;
+mod defaults {
+    pub const DEFAULT_PORT: u16 = 3000;
+    pub const DEFAULT_ADDRESS: &str = "127.0.0.1";
+    pub const CTRL_C_SEQUENCE: [u8; 5] = [255, 244, 255, 253, 6];
+}
+
+use clap_config::*;
+mod clap_config {
+    use super::*;
+
+    #[derive(Parser, Debug)]
+    pub struct CLIArg {
+        /// IP Address to connect to or start a server on
+        #[clap(long, short, default_value = DEFAULT_ADDRESS, global = true)]
+        pub address: IpAddr,
+
+        /// TCP Port to connect to or start a server on
+        #[clap(long, short, default_value_t = DEFAULT_PORT, global = true)]
+        pub port: u16,
+
+        /// Logs to stdout by default, set this flag to disable it
+        #[clap(long, short = 'd', global = true)]
+        pub log_disable: bool,
+
+        /// The subcommand to run
+        #[clap(subcommand)]
+        pub subcommand: CLISubcommand,
+    }
+
+    #[derive(Subcommand, Debug)]
+    pub enum CLISubcommand {
+        /// Start a server on the given address and port
+        Server,
+        /// Connect to a server running on the given address and port
+        Client,
+    }
+}
+
+fn main() {
+    println!("Welcome to rtelnet");
+
+    let cli_arg = CLIArg::parse();
+    let address = cli_arg.address;
+    let port = cli_arg.port;
+    let socket_address = format!("{}:{}", address, port);
+
+    if !cli_arg.log_disable {
+        femme::start()
+    }
+
+    match match cli_arg.subcommand {
+        CLISubcommand::Server => start_server(socket_address),
+        CLISubcommand::Client => start_client(socket_address),
+    } {
+        Ok(_) => {
+            println!("Program exited successfully");
+        }
+        Err(error) => {
+            println!("Program exited with an error: {}", error);
+        }
+    }
+}
+
+use server::*;
+mod server {
+    use super::*;
+
+    pub fn start_server(socket_address: String) -> IOResult<()> {
+        let tcp_listener = TcpListener::bind(socket_address)?;
+        loop {
+            log::info!("Waiting for a incoming connection...");
+            let (tcp_stream, ..) = tcp_listener.accept()?; // This is a blocking call.
+            let _ = handle_connection(tcp_stream)?;
+        }
+    }
+
+    fn handle_connection(tcp_stream: TcpStream) -> IOResult<()> {
+        log::info!("Start handle connection");
+
+        let reader = &mut BufReader::new(&tcp_stream);
+        let write = &mut BufWriter::new(&tcp_stream);
+
+        loop {
+            let mut incoming: Vec<u8> = vec![];
+
+            // Read from reader.
+            let num_bytes_read = reader.read_until(b'\n', &mut incoming)?;
+
+            // Check for EOF. The stream is closed.
+            if num_bytes_read == 0 {
+                break;
+            }
+
+            // Check for Ctrl+C.
+            if haystack_contains_needle(&incoming, &defaults::CTRL_C_SEQUENCE.to_vec()) {
+                break;
+            }
+
+            // Process.
+            let outgoing = process(&incoming);
+
+            // Write to writer.
+            write.write(&outgoing)?;
+            let _ = write.flush()?;
+
+            // Print debug.
+            log::info!("-> Rx(bytes) : {:?}", &incoming);
+            log::info!(
+                "-> Rx(string): '{}', size: {} bytes",
+                String::from_utf8_lossy(&incoming).trim(),
+                incoming.len(),
+            );
+            log::info!(
+                "<- Tx(string): '{}', size: {} bytes",
+                String::from_utf8_lossy(&outgoing).trim(),
+                outgoing.len()
+            );
+        }
+
+        log::info!("End handle connection - connection closed");
+
+        Ok(())
+    }
+
+    fn process(incoming: &Vec<u8>) -> Vec<u8> {
+        // Convert incoming to String, and remove any trailing whitespace (includes newline).
+        let incoming = String::from_utf8_lossy(incoming);
+        let incoming = incoming.trim();
+        format!("<<< echo: {}\n", incoming).as_bytes().to_vec()
+    }
+
+    fn haystack_contains_needle(haystack: &Vec<u8>, needle: &Vec<u8>) -> bool {
+        haystack
+            .windows(needle.len())
+            .any(|window| window == needle)
+    }
+}
+
+fn start_client(socket_address: String) -> IOResult<()> {
+    let tcp_stream = TcpStream::connect(socket_address)?;
+    let (mut reader, mut writer) = (BufReader::new(&tcp_stream), BufWriter::new(&tcp_stream));
+
+    loop {
+        // Read user input.
+        let outgoing = {
+            let mut it = String::new();
+            let _ = stdin().read_line(&mut it)?;
+            it.as_bytes().to_vec()
+        };
+
+        // Tx user input to writer.
+        let _ = writer.write(&outgoing)?;
+        writer.flush()?;
+
+        // Rx response from reader.
+        let incoming = {
+            let mut it = vec![];
+            let _ = reader.read_until(b'\n', &mut it);
+            it
+        };
+
+        println!("{}", String::from_utf8_lossy(&incoming).trim());
+
+        // Print debug.
+        log::info!(
+            "-> Tx: '{}', size: {} bytes",
+            String::from_utf8_lossy(&outgoing).trim(),
+            outgoing.len()
+        );
+        log::info!(
+            "<- Rx: '{}', size: {} bytes",
+            String::from_utf8_lossy(&incoming).trim(),
+            incoming.len()
+        );
+    }
+}
--- tcp-server-netcat-client/README.md
+++ tcp-server-netcat-client/README.md
@@ -3,5 +3,6 @@
 To learn more about netcat client here are some resources:
 1. [book](https://nanxiao.gitbooks.io/openbsd-netcat-demystified/content/).
 2. [book source](https://github.com/NanXiao/openbsd-netcat-demystified).
-3. [my netcat code](https://github.com/nazmulidris/rust-scratch/rcat).
+3. [my netcat code #1 - rcat](https://github.com/nazmulidris/rust-scratch/rcat).
+3. [my netcat code #2 - rtelnet](https://github.com/nazmulidris/rust-scratch/rtelnet).
 4. [category on github](https://github.com/topics/netcat?l=rust).
