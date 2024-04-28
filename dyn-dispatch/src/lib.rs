15bf60682360e2a3a793a327a40b857e9081c058
Nazmul Idris <nazmul@fasterlap.com>
2024-04-28 18:45:25 -0500

    [dyn-dispatch] Refactor code into a single file

--- dyn-dispatch/src/lib.rs
+++ dyn-dispatch/src/lib.rs
@@ -14,12 +14,164 @@
  *   See the License for the specific language governing permissions and
  *   limitations under the License.
  */
+use std::error::Error;
+use std::fmt::Display;
 
-pub mod fixtures;
-pub use fixtures::*;
+// ErrorOne.
+mod error_one {
+    use super::*;
 
-pub mod static_dispatch;
-pub use static_dispatch::*;
+    #[derive(Debug)]
+    pub struct ErrorOne;
 
-pub mod dynamic_dispatch;
-pub use dynamic_dispatch::*;
+    impl Display for ErrorOne {
+        fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+            write!(f, "ErrorOne")
+        }
+    }
+
+    impl Error for ErrorOne {}
+}
+use error_one::ErrorOne;
+
+// ErrorTwo.
+mod error_two {
+    use super::*;
+
+    #[derive(Debug)]
+    pub struct ErrorTwo;
+
+    impl Display for ErrorTwo {
+        fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+            write!(f, "ErrorTwo")
+        }
+    }
+
+    impl Error for ErrorTwo {}
+}
+use error_two::ErrorTwo;
+
+// Random boolean generator.
+pub fn random_bool() -> bool {
+    rand::random()
+}
+
+// Static dispatch.
+mod static_dispatch {
+    use super::*;
+
+    mod receives {
+        use super::*;
+
+        pub fn accept_error<E: Error>(error: E) {
+            println!("Handling ErrorOne Debug: {:?}", error);
+            println!("Handling ErrorOne Display: {}", error);
+        }
+
+        pub fn accept_error_with_syntactic_sugar(error: impl Error) {
+            println!("Handling ErrorOne Debug: {:?}", error);
+            println!("Handling ErrorOne Display: {}", error);
+        }
+    }
+
+    mod returns {
+        use super::*;
+
+        pub fn return_error_one() -> ErrorOne {
+            ErrorOne
+        }
+
+        pub fn return_error_two() -> ErrorTwo {
+            ErrorTwo
+        }
+
+        // DOES NOT WORK! Need dynamic dispatch.
+        // pub fn return_single_error() -> impl Error {
+        //     if random_bool() {
+        //         ErrorOne
+        //     } else {
+        //         ErrorTwo
+        //     }
+        // }
+
+        pub fn return_single_error() -> impl Error {
+            return ErrorOne;
+        }
+    }
+}
+
+// Dynamic dispatch.
+mod dynamic_dispatch {
+    use super::*;
+
+    mod receives {
+        use super::*;
+
+        pub fn recieve_error_by_ref(error: &dyn Error) {
+            println!("Handling Error Debug: {:?}", error);
+            println!("Handling Error Display: {}", error);
+        }
+
+        pub fn example_1() {
+            let error_one = ErrorOne;
+            recieve_error_by_ref(&error_one);
+            let error_two = ErrorTwo;
+            recieve_error_by_ref(&error_two);
+        }
+
+        pub fn receive_error_by_box(error: Box<dyn Error>) {
+            println!("Handling Error Debug: {:?}", error);
+            println!("Handling Error Display: {}", error);
+        }
+
+        pub fn example_2() {
+            let error_one = ErrorOne;
+            let it = Box::new(error_one);
+            receive_error_by_box(it);
+            let error_two = ErrorTwo;
+            receive_error_by_box(Box::new(error_two));
+        }
+
+        pub fn receive_slice_of_errors(arg: &[&dyn Error]) {
+            for error in arg {
+                println!("Handling Error Debug: {:?}", error);
+                println!("Handling Error Display: {}", error);
+            }
+        }
+    }
+
+    mod returns {
+        use super::*;
+
+        pub fn return_one_of_two_errors() -> Box<dyn Error> {
+            if random_bool() {
+                Box::new(ErrorOne)
+            } else {
+                Box::new(ErrorTwo)
+            }
+        }
+
+        pub fn return_one_of_two_errors_with_arc() -> std::sync::Arc<dyn Error> {
+            if random_bool() {
+                std::sync::Arc::new(ErrorOne)
+            } else {
+                std::sync::Arc::new(ErrorTwo)
+            }
+        }
+
+        pub fn return_slice_of_errors() -> Vec<&'static dyn Error> {
+            let mut errors: Vec<&dyn Error> = vec![];
+            if random_bool() {
+                errors.push(&(ErrorOne));
+            } else {
+                errors.push(&(ErrorTwo));
+            }
+            errors
+        }
+
+        pub fn mut_vec_containing_different_types_of_errors(mut_vec: &mut Vec<&dyn Error>) {
+            mut_vec.push(&ErrorOne);
+            mut_vec.push(&ErrorTwo);
+        }
+    }
+}
