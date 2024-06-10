5057e583732caf97324a0775f74a2dae3a98df52
Nazmul Idris <nazmul@fasterlap.com>
2024-06-10 11:53:40 -0500

    [kv_example] Update examples in tests

--- kv_example/src/miette_error_test_cases.rs
+++ kv_example/src/miette_error_test_cases.rs
@@ -18,6 +18,8 @@
 //! This module contains all the test cases for the [miette] crate. This isn't used by any
 //! other module in the project, and is only used for testing purposes.
 
+use app_use_case_into_diagnostic_and_context::UnderlyingDatabaseError;
+
 /// USE CASE 1: Lazy, don't want to make my own [miette::Diagnostic] trait implementation
 /// along with custom error types (using [thiserror]).
 ///
@@ -28,34 +30,53 @@
 /// w/ more context provided after the errors are generated, and they are getting bubbled
 /// up the call chain.
 #[cfg(test)]
-mod app_use_case_into_diagnostic_and_context {
+pub mod app_use_case_into_diagnostic_and_context {
     use crossterm::style::Stylize;
     use miette::{Context, IntoDiagnostic};
+
+    #[derive(Debug, thiserror::Error)]
+    pub enum UnderlyingDatabaseError {
+        #[error("database corrupted")]
+        DatabaseCorrupted,
+    }
+
+    fn return_error_result() -> Result<u32, std::num::ParseIntError> {
+        let error_result: Result<u32, std::num::ParseIntError> = "1.2".parse::<u32>();
+        error_result
+    }
 
     /// The errors are "unwrapped" / displayed in the opposite order in which they are
     /// added / stacked.
     ///
-    /// DISPLAY ORDER: | STACK / CREATE ORDER:
-    /// 1. Top:        | The third ".. additional context .." error.
-    /// 2. Middle:     | The second ".. database corrupted .. " error.
-    /// 3. Bottom:     | The first "parse error".
+    /// Create order / stack insertion order:
+    /// 1. The parse error `std::num::ParseIntError`, which is displayed last.
+    /// 2. The error context `🍍 foo bar baz`, which is displayed 2nd from last.
+    /// 3. The custom string error `custom string error`, which is displayed 3rd from last.
+    /// 4. The entity not found error `std::io::ErrorKind::NotFound`, which is displayed 4th from last.
+    /// 5. The database corrupted error `rkv::StoreError::DatabaseCorrupted`, which is displayed 5th from last.
+    /// 6. The additional context error `🎃 this is additional context about the failure`, which is displayed first.
     ///
-    /// Here's the actual output:
-    /// ```
-    /// Error:   × 🎃 this is additional context about the failure
-    /// ├─▶ database corrupted
-    /// ├─▶ 🍍 foo bar baz
-    /// ╰─▶ invalid digit found in string
+    /// Here's the actual output showing the inverted display order:
+    /// ```text
+    /// Err(  × 🎃 this is additional context about the failure
+    ///   ├─▶ database corrupted
+    ///   ├─▶ entity not found
+    ///   ├─▶ custom string error
+    ///   ├─▶ 🍍 foo bar baz
+    ///   ╰─▶ invalid digit found in string
+    /// )
     /// ```
     #[test]
     fn test_into_diagnostic() -> miette::Result<()> {
-        let result: Result<u32, std::num::ParseIntError> = "1.2".parse::<u32>();
-        assert!(result.is_err());
+        let error_result = return_error_result();
+        assert!(error_result.is_err());
 
-        let new_miette_result = result
+        let new_miette_result = error_result
             .into_diagnostic()
             .context("🍍 foo bar baz")
-            .wrap_err(rkv::StoreError::DatabaseCorrupted)
+            .wrap_err(miette::miette!("custom string error"))
+            .wrap_err(std::io::ErrorKind::NotFound)
+            .wrap_err(UnderlyingDatabaseError::DatabaseCorrupted)
             .wrap_err("🎃 this is additional context about the failure");
 
         assert!(new_miette_result.is_err());
@@ -84,9 +105,19 @@ mod app_use_case_into_diagnostic_and_context {
         // Third.
         assert_eq!(
             iter.next().map(|it| it.to_string()).unwrap(),
-            "🍍 foo bar baz"
+            "entity not found"
         );
         // Fourth.
+        assert_eq!(
+            iter.next().map(|it| it.to_string()).unwrap(),
+            "custom string error"
+        );
+        // Fifth.
+        assert_eq!(
+            iter.next().map(|it| it.to_string()).unwrap(),
+            "🍍 foo bar baz"
+        );
+        // Sixth.
         assert_eq!(
             iter.next().map(|it| it.to_string()).unwrap(),
             "invalid digit found in string"
@@ -95,25 +126,34 @@ mod app_use_case_into_diagnostic_and_context {
         Ok(())
     }
 
-    /// It is possible to convert from a [miette::Report] into an [Box]ed [std::error::Error].
+    /// It is possible to convert from a [miette::Report] into an [Box]'d [std::error::Error].
     #[test]
     fn test_convert_report_into_error() -> std::result::Result<(), Box<dyn std::error::Error>> {
-        let result = "1.2"
-            .parse::<u32>()
-            .into_diagnostic()
-            .wrap_err(miette::Report::msg(
-                "wrapper for the source parse int error",
-            ));
+        let miette_result: miette::Result<u32> =
+            return_error_result()
+                .into_diagnostic()
+                .wrap_err(miette::Report::msg(
+                    "wrapper for the source parse int error",
+                ));
 
-        match result {
-            Ok(_) => {
-                return Ok(());
-            }
-            Err(miette_report) => {
-                let it: Box<dyn std::error::Error> = miette_report.into();
-                return Err(it);
-            }
-        }
+        let converted_result: std::result::Result<(), Box<dyn std::error::Error>> =
+            match miette_result {
+                Ok(_) => Ok(()),
+                Err(miette_report) => {
+                    let error: Box<dyn std::error::Error> = miette_report.into();
+                    Err(error)
+                }
+            };
+
+        println!(
+            "{}:\n{:?}\n",
+            "debug output".blue().bold(),
+            converted_result
+        );
+
+        assert!(converted_result.is_err());
+
+        Ok(())
     }
 }
 
@@ -139,9 +179,9 @@ mod app_use_case_into_diagnostic_and_context {
 ///    help links for each error variant. The report is automatically generated by miette,
 ///    and you can customize if you like.
 #[derive(thiserror::Error, Debug, miette::Diagnostic)]
-enum RkvError {
+enum KVStoreError {
     #[diagnostic(
-        code(rkv::DatabaseError::FileSystemError),
+        code(MyErrorCode::FileSystemError),
         help("https://docs.rs/rkv/latest/rkv/enum.StoreError.html"),
         // url(docsrs) /* Works if this code was on crates.io / docs.rs */
     )]
@@ -149,14 +189,14 @@ enum RkvError {
     CouldNotCreateDbFolder { db_folder_path: String },
 
     #[diagnostic(
-        code(rkv::DatabaseError::StoreCreationOrAccessError),
+        code(MyErrorCode::StoreCreationOrAccessError),
         help("https://docs.rs/rkv/latest/rkv/enum.StoreError.html"),
         // url(docsrs) /* Works if this code was on crates.io / docs.rs */
     )]
     #[error("💾 Could not get or create environment, or open store")]
     CouldNotGetOrCreateEnvironmentOrOpenStore {
         #[from]
-        source: rkv::StoreError,
+        source: UnderlyingDatabaseError,
     },
 }
 
@@ -172,30 +212,22 @@ mod library_use_case_fails_tests_miette_thiserror {
     use pretty_assertions::assert_eq;
 
     // Flat error.
-    fn throws_flat_error_db() -> miette::Result<(), RkvError> {
-        Err(RkvError::CouldNotCreateDbFolder {
+    fn return_flat_error_db() -> miette::Result<(), KVStoreError> {
+        Err(KVStoreError::CouldNotCreateDbFolder {
             db_folder_path: "some/path/to/db".into(),
         })
     }
 
-    // Nested error.
-    fn throws_nested_error_store() -> miette::Result<(), RkvError> {
-        let store_error = rkv::StoreError::DatabaseCorrupted;
-        let rkv_error = RkvError::from(store_error);
-        Err(rkv_error)
-
-        // Same as above.
-        // Err(rkv::StoreError::DatabaseCorrupted.into())
-
-        // Same as above.
-        // Err(RkvError::CouldNotGetOrCreateEnvironmentOrOpenStore {
-        //     source: rkv::StoreError::DatabaseCorrupted,
-        // })
-    }
-
+    /// Expected output:
+    /// ```text
+    /// Error: MyErrorCode::FileSystemError
+    ///
+    /// × 📂 Could not create db folder: 'some/path/to/db' on disk
+    /// help: https://docs.rs/rkv/latest/rkv/enum.StoreError.html
+    /// ```
     #[test]
     fn fails_with_flat_error() -> miette::Result<()> {
-        let result = throws_flat_error_db();
+        let result = return_flat_error_db();
         if let Err(rkv_error) = &result {
             assert_eq!(
                 format!("{:?}", rkv_error),
@@ -210,9 +242,32 @@ mod library_use_case_fails_tests_miette_thiserror {
         Ok(())
     }
 
+    // Nested error.
+    fn return_nested_error_store() -> miette::Result<(), KVStoreError> {
+        // Variant 1 - Very verbose.
+        let store_error = UnderlyingDatabaseError::DatabaseCorrupted;
+        let rkv_error = KVStoreError::from(store_error);
+        Err(rkv_error)
+
+        // Variant 2 - Same as above.
+        // Err(UnderlyingDatabaseError::DatabaseCorrupted.into())
+
+        // Variant 3 - Same as above.
+        // Err(KVStoreError::CouldNotGetOrCreateEnvironmentOrOpenStore {
+        //     source: UnderlyingDatabaseError::DatabaseCorrupted,
+        // })
+    }
+
+    /// Expected output:
+    /// ```text
+    /// Error: MyErrorCode::StoreCreationOrAccessError
+    ///   × 💾 Could not get or create environment, or open store
+    ///   ╰─▶ database corrupted
+    ///   help: https://docs.rs/rkv/latest/rkv/enum.StoreError.html
+    /// ```
     #[test]
     fn fails_with_nested_error() -> miette::Result<()> {
-        let result = throws_nested_error_store();
+        let result = return_nested_error_store();
         if let Err(rkv_error) = &result {
             assert_eq!(
                 format!("{:?}", rkv_error),
