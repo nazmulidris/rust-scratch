328f5e71b4d1c58dfb085a05a0525c5f92c0ed26
Nazmul Idris <nazmul@fasterlap.com>
2024-12-06 10:52:08 -0600

    [tls] Make gen-cert.rs work
    - Add comprehensive error handling in scripting.rs

--- tls/src/bin/gen-certs.rs
+++ tls/src/bin/gen-certs.rs
@@ -15,47 +15,41 @@
  *   limitations under the License.
  */
 
+use constants::*;
 use crossterm::style::Stylize as _;
-use r3bl_core::ok;
-use std::{env, path::PathBuf, process::Command};
+use r3bl_core::{ok, with};
+use std::{
+    env,
+    process::Command,
+    sync::{Arc, Mutex},
+};
 use strum_macros::{Display, EnumString};
-use tls::scripting::{
-    self,
-    directory::{self, MkdirOptions},
-    download::download_file_overwrite_existing,
-    environment, github_api,
-    path::{self},
-    permissions,
+use tls::{
+    directory_stack::DirStack,
+    fs_path::{self, try_pwd},
+    fs_paths, fs_paths_exist,
+    github_api::{Separator, UrlBuilder},
+    scripting::{
+        directory_create::{self, MkdirOptions},
+        download::try_download_file_overwrite_existing,
+        environment, github_api, permissions,
+    },
 };
 
-#[derive(Display, EnumString)]
-pub enum FolderLocations {
-    #[strum(serialize = "bin")]
-    BinFolder,
+pub mod constants {
+    pub const CERTS_DIR: &str = "certs";
+    pub const BIN_DIR: &str = "bin";
+    pub const GENERATED_DIR: &str = "generated";
 
-    #[strum(serialize = "generated")]
-    GeneratedFolder,
-}
-
-#[derive(Display, EnumString)]
-pub enum BinaryFileName {
-    #[strum(serialize = "cfssl")]
-    CFSSL,
-
-    #[strum(serialize = "cfssljson")]
-    CFSSLJSON,
+    pub const CFSSL_BIN: &str = "cfssl";
+    pub const CFSSLJSON_BIN: &str = "cfssljson";
 
     #[cfg(all(target_os = "linux", target_arch = "x86_64"))]
-    #[strum(serialize = "linux_amd64")]
-    OsArch,
-
+    pub const OS_ARCH: &str = "linux_amd64";
     #[cfg(all(target_os = "macos", target_arch = "aarch64"))]
-    #[strum(serialize = "darwin_arm64")]
-    OsArch,
-
+    pub const OS_ARCH: &str = "darwin_arm64";
     #[cfg(all(target_os = "windows", target_arch = "x86_64"))]
-    #[strum(serialize = "windows_amd64.exe")]
-    OsArch,
+    pub const OS_ARCH: &str = "windows_amd64.exe";
 }
 
 #[derive(Display, EnumString)]
@@ -66,19 +60,13 @@ pub enum GithubLocation {
     Repo,
 }
 
-impl TryFrom<FolderLocations> for PathBuf {
-    type Error = miette::Error;
-
-    /// Returns the fully qualified path of the location. Eg: not just `bin`, but
-    /// `/home/user/some/path/bin`, where the prefix or root of this path is the current
-    /// working directory of the process.
-    fn try_from(value: FolderLocations) -> Result<Self, Self::Error> {
-        let path = path::fq_path_relative_to_pwd(value.to_string())?;
-        ok!(path)
+#[tokio::main]
+async fn main() -> miette::Result<()> {
+    // Ensure that the current working directory is the `tls` crate.
+    if !try_pwd()?.ends_with("tls") {
+        miette::bail!("You might be in the wrong folder; please run this in the root folder of the `tls` crate");
     }
-}
 
-fn main() -> miette::Result<()> {
     // Setup tracing.
     tracing_subscriber::fmt()
         .with_max_level(tracing::Level::DEBUG)
@@ -87,123 +75,142 @@ fn main() -> miette::Result<()> {
         .without_time()
         .init();
 
-    tracing::debug!("pwd at start" = ?path::pwd());
+    tracing::debug!("pwd at start" = ?fs_path::try_pwd());
 
-    // Add to PATH.
-    let my_path = environment::get_path_prefixed(PathBuf::try_from(FolderLocations::BinFolder)?)?;
+    // Add to PATH (./certs/bin).
+    let path_to_cfssl_bin = tls::fs_paths!( with_root: try_pwd()?, CERTS_DIR, BIN_DIR );
+    let my_path = environment::try_get_path_prefixed(path_to_cfssl_bin)?;
 
-    tracing::debug!("my_path" = %format!("{:.50}{}", my_path, "...<clip>".red()));
+    let dir_stack = &mut DirStack::get_mut_singleton()?;
 
-    // 00: remove comments & write rust version of that script
-    // If cfssl or cfssljson files do not exist, download them.
-    // Command::new("fish")
-    //     .env("PATH", my_path)
-    //     .arg("get-cfssl-binaries.fish")
-    //     .status()
-    //     .expect("Failed to execute get-cfssl-binaries.fish");
-
-    // If `generated` directory exists, delete it. And create a new one.
-    scripting::directory::mkdir(
-        PathBuf::try_from(FolderLocations::GeneratedFolder)?,
-        MkdirOptions::CreateIntermediateFoldersAndPurgeExisting,
-    )?;
-
-    // Generate CA and server certificates in the `generated` directory.
-    let dir_stack = &mut scripting::directory_stack::Stack::get_mut_singleton()?;
-    _ = dir_stack
-        .lock()
-        .unwrap()
-        .pushd(PathBuf::try_from(FolderLocations::GeneratedFolder)?)?;
-
-    tracing::debug!("pwd after pushd" = ?path::pwd());
+    download_cfssl_binaries(dir_stack).await?;
 
     // 00: remove comments below
-    // generate_certs();
+    // generate_certs_using_cfssl_bin(dir_stack, &my_path)?;
 
     // 00: remove comments below
-    // display_status();
-
-    _ = dir_stack.lock().unwrap().popd()?;
-
-    tracing::debug!("pwd after popd" = ?path::pwd());
+    // display_status_using_openssl_bin(dir_stack, &my_path)?;
 
     ok!()
 }
 
-async fn download_cfssl_binaries() -> miette::Result<()> {
-    // If cfssl or cfssljson files do not exist, download them.
-    let (bin_folder, cfssl_file, cfssljson_file) = {
-        let bin_folder = PathBuf::try_from(FolderLocations::BinFolder)?;
-        let cfssl_file = bin_folder.join(BinaryFileName::CFSSL.to_string());
-        let cfssljson_file = bin_folder.join(BinaryFileName::CFSSLJSON.to_string());
-        (bin_folder, cfssl_file, cfssljson_file)
-    };
+async fn download_cfssl_binaries(dir_stack: &mut Arc<Mutex<DirStack>>) -> miette::Result<()> {
+    let bin_folder = fs_paths!(new: CERTS_DIR, BIN_DIR);
+    with!(
+        &bin_folder,
+        as root,
+        run {
+            // Early return if the `certs/bin` directory & files exist.
+            let cfssl_file = fs_paths!(with_root: root, CFSSL_BIN);
+            let cfssljson_file = fs_paths!(with_root: root, CFSSLJSON_BIN);
+            if fs_paths_exist!(&root, &cfssl_file, &cfssljson_file) {
+                println!(
+                    "🎉 {} and {} binaries already exist.",
+                    cfssl_file.display().to_string().magenta(),
+                    cfssljson_file.display().to_string().magenta()
+                );
+                return ok!();
+            };
+        }
+    );
 
-    // Early return if binaries already exist.
-    if cfssl_file.exists() && cfssljson_file.exists() {
-        println!(
-            "🎉 {} and {} binaries already exist.",
-            cfssl_file.display().to_string().magenta(),
-            cfssljson_file.display().to_string().magenta()
-        );
-        return ok!();
-    }
-
-    println!("📦 Downloading binaries...");
-    directory::mkdir(
-        bin_folder,
-        MkdirOptions::CreateIntermediateFoldersAndPurgeExisting,
+    // Create a new `certs/bin` directory.
+    directory_create::try_mkdir(
+        &bin_folder,
+        MkdirOptions::CreateIntermediateDirectoriesAndPurgeExisting,
     )?;
 
-    // Get latest release tag for the binaries from their GitHub repo.
+    // Pushd into the `certs/bin` directory.
+    let (
+        /* this gets dropped immediately, doesn't bind */ _,
+        /* this gets dropped like a "normal" variable" */ _dir_stack_drop_handle,
+    ) = dir_stack.lock().unwrap().try_pushd(&bin_folder)?;
+
+    // Try to get latest release tag for the binaries from their GitHub repo.
     let (cfssl_bin_url, cfssljson_bin_url) = {
         let org = &GithubLocation::Org.to_string();
         let repo = &GithubLocation::Repo.to_string();
-        let release_version = github_api::get_latest_release_tag_from_github(org, repo).await?;
-        let root_url = format!("https://github.com/{}/{}/releases/download", org, repo);
-        let cfssl_bin_url = format!(
-            "{}/v{}/{}_{}_{}",
-            root_url,
-            release_version,
-            BinaryFileName::CFSSL,
-            release_version,
-            BinaryFileName::OsArch
+        let release_version =
+            &github_api::try_get_latest_release_tag_from_github(org, repo).await?;
+
+        let root_url = UrlBuilder::default()
+            + "https://github.com/"
+            + org
+            + Separator::ForwardSlash
+            + repo
+            + "/releases/download";
+
+        let cfssl_bin_url = &root_url
+            + "/v"
+            + release_version
+            + Separator::ForwardSlash
+            + CFSSL_BIN
+            + Separator::Underscore
+            + release_version
+            + Separator::Underscore
+            + OS_ARCH;
+
+        let cfssljson_bin_url = &root_url
+            + "/v"
+            + release_version
+            + Separator::ForwardSlash
+            + CFSSLJSON_BIN
+            + Separator::Underscore
+            + release_version
+            + Separator::Underscore
+            + OS_ARCH;
+
+        // Print the latest URLs of the binaries.
+        println!("🌐 URLs of latest versions of binaries...");
+        println!("💾 {}: {}", stringify!(cfssl_bin_url), cfssl_bin_url);
+        println!(
+            "💾 {}: {}",
+            stringify!(cfssljson_bin_url),
+            cfssljson_bin_url
         );
-        let cfssljson_bin_url = format!(
-            "{}/v{}/{}_{}_{}",
-            root_url,
-            release_version,
-            BinaryFileName::CFSSLJSON,
-            release_version,
-            BinaryFileName::OsArch
-        );
-        (cfssl_bin_url, cfssljson_bin_url)
+
+        (cfssl_bin_url.to_string(), cfssljson_bin_url.to_string())
     };
 
-    // Print variables.
-    println!("💾 {}: {}", stringify!(cfssl_bin_url), cfssl_bin_url);
-    println!("💾 {}: {}", stringify!(cfssljson), cfssljson_bin_url);
+    // Download the binaries into the `certs/bin` directory.
+    println!(
+        "📦 Downloading binaries {}, {} to {} ...",
+        CFSSL_BIN.blue(),
+        CFSSLJSON_BIN.blue(),
+        try_pwd()?.display().to_string().magenta()
+    );
 
-    // Download binaries.
-    download_file_overwrite_existing(&cfssl_bin_url, &cfssl_file).await?;
-    download_file_overwrite_existing(&cfssljson_bin_url, &cfssljson_file).await?;
+    try_download_file_overwrite_existing(&cfssl_bin_url, CFSSL_BIN).await?;
+    try_download_file_overwrite_existing(&cfssljson_bin_url, CFSSLJSON_BIN).await?;
 
     // Make them executable.
-    permissions::set_file_executable(&cfssl_file)?;
-    permissions::set_file_executable(&cfssljson_file)?;
+    permissions::try_set_file_executable(CFSSL_BIN)?;
+    permissions::try_set_file_executable(CFSSLJSON_BIN)?;
 
     // Display success message.
     println!(
         "🎉 Downloaded {} and {} executable binaries to: {}",
-        &cfssl_file.display().to_string().magenta(),
-        &cfssljson_file.display().to_string().magenta(),
-        path::pwd()?.display().to_string().magenta()
+        CFSSL_BIN.magenta(),
+        CFSSLJSON_BIN.magenta(),
+        fs_path::try_pwd()?.display().to_string().magenta()
     );
 
     ok!()
 }
 
-fn generate_certs() {
+/// This function expects the `pwd` to be the root directory of the crate.
+fn generate_certs_using_cfssl_bin(
+    dir_stack: &mut Arc<Mutex<DirStack>>,
+    my_path: &str,
+) -> miette::Result<()> {
+    // Pushd into the `certs/generated` directory. Generate CA and server certificates.
+    _ = dir_stack
+        .lock()
+        .unwrap()
+        .try_pushd(fs_paths!(new: CERTS_DIR, GENERATED_DIR))?;
+
+    tracing::debug!("pwd after pushd" = ?fs_path::try_pwd());
+
     // Generate root certificate (CA) and sign it.
     Command::new("cfssl")
         .args(["gencert", "-initca", "../config/ca-csr.json"])
@@ -236,9 +243,24 @@ fn generate_certs() {
         .args(["-bare", "server"])
         .output()
         .expect("Failed to execute cfssljson for server");
+
+    ok!()
 }
 
-fn display_status() {
+// 00: if openssl is not installed, then handle install it using brew (add to scripting.rs)
+/// This function expects the `pwd` to be the root directory of the crate.
+fn display_status_using_openssl_bin(
+    dir_stack: &mut Arc<Mutex<DirStack>>,
+    my_path: &str,
+) -> miette::Result<()> {
+    // Pushd into the `certs/generated` directory. Generate CA and server certificates.
+    _ = dir_stack
+        .lock()
+        .unwrap()
+        .try_pushd(fs_paths!(new: CERTS_DIR, GENERATED_DIR))?;
+
+    tracing::debug!("pwd after pushd" = ?fs_path::try_pwd());
+
     println!(
         "\x1b[32m🎉 Generated certificates in the \x1b[33m{}\x1b[0m directory.",
         env::current_dir().unwrap().display()
@@ -265,4 +287,6 @@ fn display_status() {
     } else {
         println!("\x1b[31m❗ Certificates are invalid\x1b[0m");
     }
+
+    ok!()
 }
