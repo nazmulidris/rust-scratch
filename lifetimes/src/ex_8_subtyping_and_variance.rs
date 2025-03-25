a6ebb369e140dbb7b487a6acd79b441ae2994b97
Nazmul Idris <nazmul@fasterlap.com>
2025-03-25 09:12:24 -0500

    [lifetimes] Update 8th example, showing complex subtyping, no lifetimes

--- lifetimes/src/ex_8_subtyping_and_variance.rs
+++ lifetimes/src/ex_8_subtyping_and_variance.rs
@@ -22,36 +22,72 @@
 mod tests {
     use std::fmt::Debug;
 
-    /* Simple concrete types. */
+    //
+    // Concrete types (simple).
+    //
 
-    #[derive(Clone, Copy, Debug, PartialEq, Eq)]
+    #[derive(Debug, Clone, Copy, PartialEq, Eq)]
+    pub struct Width(pub usize);
+
+    #[derive(Debug, Clone, Copy, PartialEq, Eq)]
+    pub struct Height(pub usize);
+
+    #[derive(Debug, Clone, Copy, PartialEq, Eq)]
+    pub struct X(pub usize);
+
+    #[derive(Debug, Clone, Copy, PartialEq, Eq)]
+    pub struct Y(pub usize);
+
+    #[derive(Debug, Clone, Copy, PartialEq, Eq)]
     pub struct Dimens2D {
+        pub width: Width,
+        pub height: Height,
+        pub x: X,
+        pub y: Y,
+    }
+
+    pub struct Dimens2DAlt {
         pub width: usize,
         pub height: usize,
         pub x: usize,
         pub y: usize,
     }
 
-    /* Traits and their subtyping relationships. */
+    //
+    // Traits.
+    //
 
-    pub trait View<T: Copy + Debug> {
-        fn get_dimens(&self) -> T;
-        fn set_dimens(&mut self, dimens: T);
+    /// Super trait.
+    pub trait View<InnerStorage: Debug + Copy + Clone + PartialEq + Eq> {
+        fn get_dimens(&self) -> InnerStorage;
+        fn set_dimens(&mut self, dimens: InnerStorage);
     }
 
-    pub trait Component<T: Copy + Debug>: View<T> {
-        fn get_as_view(&self) -> &dyn View<T>;
-        fn get_as_view_mut(&mut self) -> &mut dyn View<T>;
-        fn render(&self);
+    /// Sub trait.
+    pub trait Component<
+        InnerStorage: Debug + Copy + Clone + PartialEq + Eq,
+        RenderOutput: Debug + Clone,
+    >: View<InnerStorage>
+    {
+        fn render(&self) -> RenderOutput;
     }
 
-    /* Trait implementations. */
+    /// Redundant.
+    pub trait ComponentAlt<T: Debug + Copy + Clone + PartialEq + Eq, O: Debug + Clone>:
+        View<T>
+    {
+        fn render(&self) -> O;
+        fn as_view(&self) -> &dyn View<T>;
+        fn as_view_mut(&mut self) -> &mut dyn View<T>;
+    }
 
-    /// Concrete type [Button2D] that implements the [View] & [Component] traits.
+    //
+    // Trait impl.
+    //
+
     pub mod button {
         use super::*;
 
-        #[derive(Clone, Copy, Debug)]
         pub struct Button2D {
             pub dimens: Dimens2D,
         }
@@ -66,26 +102,19 @@ mod tests {
             }
         }
 
-        impl Component<Dimens2D> for Button2D {
-            fn get_as_view(&self) -> &dyn View<Dimens2D> {
-                self
-            }
-
-            fn get_as_view_mut(&mut self) -> &mut dyn View<Dimens2D> {
-                self
-            }
-
-            fn render(&self) {
-                println!("Button2D: {:?}", self.dimens);
+        impl Component<Dimens2D, String> for Button2D {
+            fn render(&self) -> String {
+                format!(
+                    "Button2D: width: {}, height: {}, x: {}, y: {}",
+                    self.dimens.width.0, self.dimens.height.0, self.dimens.x.0, self.dimens.y.0
+                )
             }
         }
     }
 
-    /// Concrete type [Label2D] that implements the [View] & [Component] traits.
     pub mod label {
         use super::*;
 
-        #[derive(Clone, Copy, Debug)]
         pub struct Label2D {
             pub dimens: Dimens2D,
         }
@@ -100,148 +129,176 @@ mod tests {
             }
         }
 
-        impl Component<Dimens2D> for Label2D {
-            fn get_as_view(&self) -> &dyn View<Dimens2D> {
-                self
-            }
-
-            fn get_as_view_mut(&mut self) -> &mut dyn View<Dimens2D> {
-                self
-            }
-
-            fn render(&self) {
-                println!("Label2D: {:?}", self.dimens);
+        impl Component<Dimens2D, String> for Label2D {
+            fn render(&self) -> String {
+                format!(
+                    "Label2D: width: {}, height: {}, x: {}, y: {}",
+                    self.dimens.width.0, self.dimens.height.0, self.dimens.x.0, self.dimens.y.0
+                )
             }
         }
     }
 
-    /* Functions that work with the subtrait. */
+    //
+    // fns that work with the sub trait. (Component)
+    //
 
-    fn component_render<T: Copy + Debug>(component: &dyn Component<T>) {
-        component.render();
+    pub mod subtrait_fns {
+        use super::*;
+
+        pub fn comp_render<T: Debug + Copy + Clone + PartialEq + Eq, O: Debug + Clone>(
+            arg: &dyn Component<T, O>,
+        ) -> O {
+            arg.render()
+        }
+
+        pub fn comp_get_size<T: Debug + Copy + Clone + PartialEq + Eq, O: Debug + Clone>(
+            arg: impl Component<T, O>,
+        ) -> T {
+            arg.get_dimens()
+        }
+
+        pub fn comp_set_size<T: Debug + Copy + Clone + PartialEq + Eq, O: Debug + Clone>(
+            arg: &mut dyn Component<T, O>,
+            dimens: T,
+        ) {
+            arg.set_dimens(dimens);
+        }
     }
 
-    fn component_get_size<T: Copy + Debug>(component: &dyn Component<T>) -> T {
-        component.get_as_view().get_dimens()
+    //
+    // fns that work with the super trait. (View)
+    //
+
+    pub mod supertrait_fns {
+        use super::*;
+
+        pub fn view_get_size<T: Debug + Copy + Clone + PartialEq + Eq>(arg: impl View<T>) -> T {
+            arg.get_dimens()
+        }
+
+        pub fn view_set_size<T: Debug + Copy + Clone + PartialEq + Eq>(
+            arg: &mut dyn View<T>,
+            dimens: T,
+        ) {
+            arg.set_dimens(dimens);
+        }
+
+        // pub fn view_render<T: Debug + Copy + Clone + PartialEq + Eq, O: Debug + Clone>(
+        //     _arg: impl View<T>,
+        // ) -> O {
+        //     panic!("Render method not implemented for View trait");
+        // }
     }
-
-    fn component_set_size<T: Copy + Debug>(component: &mut dyn Component<T>, dimens: T) {
-        component.get_as_view_mut().set_dimens(dimens);
-    }
-
-    /* Functions that work with the supertrait. */
-
-    fn view_get_size<T: Copy + Debug>(view: &dyn View<T>) -> T {
-        view.get_dimens()
-    }
-
-    fn view_set_size<T: Copy + Debug>(view: &mut dyn View<T>, dimens: T) {
-        view.set_dimens(dimens);
-    }
-
-    /* Tests. */
 
     const SMALL: Dimens2D = Dimens2D {
-        width: 100,
-        height: 50,
-        x: 10,
-        y: 20,
+        width: Width(100),
+        height: Height(50),
+        x: X(10),
+        y: Y(20),
     };
 
     const LARGE: Dimens2D = Dimens2D {
-        width: 200,
-        height: 100,
-        x: 30,
-        y: 40,
+        width: Width(200),
+        height: Height(100),
+        x: X(30),
+        y: Y(40),
+    };
+
+    const OTHER: Dimens2D = Dimens2D {
+        width: Width(300),
+        height: Height(150),
+        x: X(50),
+        y: Y(60),
     };
 
     #[test]
-    fn test_access_as_component() {
-        let mut button_component = button::Button2D { dimens: SMALL };
-        let mut label_component = label::Label2D { dimens: SMALL };
+    fn test_subtrait_fns() {
+        let button_component = button::Button2D { dimens: SMALL };
+        let label_component = label::Label2D { dimens: LARGE };
 
-        component_render(&button_component);
-        component_render(&label_component);
+        let btn_output = subtrait_fns::comp_render(&button_component);
+        let lbl_output = subtrait_fns::comp_render(&label_component);
 
-        let button_size = component_get_size(&button_component);
-        let label_size = component_get_size(&label_component);
+        println!("{btn_output}");
+        println!("{lbl_output}");
 
-        assert_eq!(button_size, button_component.dimens);
-        assert_eq!(label_size, label_component.dimens);
-
-        component_set_size(&mut button_component, LARGE);
-        component_set_size(&mut label_component, LARGE);
-
-        assert_eq!(button_component.dimens, LARGE,);
-        assert_eq!(label_component.dimens, LARGE,);
+        assert_eq!(button_component.get_dimens(), SMALL);
+        assert_eq!(label_component.get_dimens(), LARGE);
     }
 
     #[test]
-    fn test_access_component_as_view() {
-        let mut button_component = button::Button2D { dimens: SMALL };
-        let mut label_component = label::Label2D { dimens: SMALL };
+    fn test_supertrait_fns() {
+        let button_component = button::Button2D { dimens: SMALL };
+        let label_component = label::Label2D { dimens: LARGE };
 
-        let button_size = button_component.get_as_view().get_dimens();
-        let label_size = button_component.get_as_view().get_dimens();
+        let btn_size = supertrait_fns::view_get_size(button_component);
+        let lbl_size = supertrait_fns::view_get_size(label_component);
 
-        assert_eq!(button_size, button_component.dimens);
-        assert_eq!(label_size, label_component.dimens);
-
-        button_component.get_as_view_mut().set_dimens(LARGE);
-        label_component.get_as_view_mut().set_dimens(LARGE);
-
-        assert_eq!(button_component.dimens, LARGE);
-        assert_eq!(label_component.dimens, LARGE);
+        assert_eq!(btn_size, SMALL);
+        assert_eq!(lbl_size, LARGE);
     }
 
     #[test]
-    fn test_access_as_view() {
+    fn test_access_comp_as_view() {
         let mut button_component = button::Button2D { dimens: SMALL };
         let mut label_component = label::Label2D { dimens: SMALL };
 
-        let button_size = view_get_size(&button_component);
-        let label_size = view_get_size(&label_component);
+        // View trait methods.
+        button_component.set_dimens(LARGE);
+        label_component.set_dimens(LARGE);
 
-        assert_eq!(button_size, button_component.dimens);
-        assert_eq!(label_size, label_component.dimens);
+        assert_eq!(button_component.get_dimens(), LARGE);
+        assert_eq!(label_component.get_dimens(), LARGE);
 
-        view_set_size(&mut button_component, LARGE);
-        view_set_size(&mut label_component, LARGE);
+        // Component trait methods.
+        let btn_output = button_component.render();
+        let lbl_output = label_component.render();
 
-        assert_eq!(button_component.dimens, LARGE);
-        assert_eq!(label_component.dimens, LARGE);
+        println!("{btn_output}");
+        println!("{lbl_output}");
     }
 
     #[test]
-    fn test_accumulating_components_in_collection() {
-        let mut components: Vec<&mut dyn Component<Dimens2D>> = vec![];
+    fn test_acc_comp_in_collection() {
+        let mut components: Vec<&mut dyn Component<Dimens2D, String>> = vec![];
 
-        let mut button_component = button::Button2D { dimens: SMALL };
-        let mut label_component = label::Label2D { dimens: SMALL };
+        // Before type erasure.
+        let button_component = &mut button::Button2D { dimens: SMALL };
+        let label_component = &mut label::Label2D { dimens: LARGE };
 
-        components.push(&mut button_component);
-        components.push(&mut label_component);
+        assert_eq!(button_component.get_dimens(), SMALL);
+        assert_eq!(label_component.get_dimens(), LARGE);
 
+        components.push(button_component);
+        components.push(label_component);
+
+        // After type erasure.
         assert_eq!(components[0].get_dimens(), SMALL);
-        assert_eq!(components[1].get_dimens(), SMALL);
-
-        // Access as a component.
-        for component in components.iter() {
-            component.render();
-        }
-
-        // Access as a view.
-        for component in components.iter() {
-            let size = component.get_dimens();
-            println!("Size: {:?}", size);
-        }
-
-        // Mutate as view.
-        for component in components.iter_mut() {
-            component.set_dimens(LARGE);
-        }
-
-        assert_eq!(components[0].get_dimens(), LARGE);
         assert_eq!(components[1].get_dimens(), LARGE);
+
+        // Access components as Component.
+        for component in components.iter() {
+            let output = component.render();
+            println!("{output}");
+        }
+
+        // Access components as View.
+        for view in components.iter() {
+            let dimens = view.get_dimens();
+            println!(
+                "Width: {}, Height: {}, X: {}, Y: {}",
+                dimens.width.0, dimens.height.0, dimens.x.0, dimens.y.0
+            );
+        }
+
+        // Set new dimens (on all the components, which are also views) in the collection.
+        for view in components.iter_mut() {
+            view.set_dimens(OTHER);
+        }
+
+        // Check if the dimens is set correctly.
+        assert_eq!(components[0].get_dimens(), OTHER);
+        assert_eq!(components[1].get_dimens(), OTHER);
     }
 }
